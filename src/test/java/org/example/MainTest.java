3f53aa5cc7678da7955b7c08ed209eaf27c780de
GatoMiau03 <d.benaldo03@ufromail.cl>
2024-04-09 23:36:53 -0400

    Archivo de los test creado

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void hacerCadaLetraMinuscula() {
+    }
+
+    @Test
+    void hacerCadenaIdonea() {
+    }
+
+    @Test
+    void esPalindromo() {
+    }
+
+    @Test
+    void imprimir() {
+    }
+}
